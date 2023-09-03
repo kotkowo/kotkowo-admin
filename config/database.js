91052a394176d997239a3e4b98de6b8589920814
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-09-03 20:31:55 +0200

    :whale: Setup docker

--- config/database.js
+++ config/database.js
@@ -12,24 +12,10 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
+        socketPath: env("DATABASE_SOCKET_PATH", "/var/run/postgresql"),
         database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        // password: env("DATABASE_PASSWORD", "postgres"),
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
