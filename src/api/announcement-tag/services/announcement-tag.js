aad96a113ebb5add46c99675d03f17e4dd9e835e
RustySnek <jodlowskipascal@gmail.com>
2023-09-04 23:24:57 +0200

    :sparkles: Add announcements and announcement-tag

---
+++ src/api/announcement-tag/services/announcement-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * announcement-tag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::announcement-tag.announcement-tag');
