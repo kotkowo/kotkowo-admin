fa01d613d54220306a8737f43f5448218bcc172b
RustySnek <jodlowskipascal@gmail.com>
2023-09-17 14:16:18 +0200

    :sparkles: Add adopted cats

---
+++ src/api/adopted-cat/routes/adopted-cat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adopted-cat router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::adopted-cat.adopted-cat');
