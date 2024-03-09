1fc7565b8386693f62eda8a4d00610812d3ec8ba
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-09 05:23:11 +0100

    🗃️ Change dev db location
    Now DB with test data should be included

--- config/database.js
+++ config/database.js
@@ -23,11 +23,7 @@ module.exports = ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
+        filename: path.join(__dirname, "..", "data.sqlite"),
       },
       useNullAsDefault: true,
     },
