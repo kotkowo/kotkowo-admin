ba76a4e0ccd2923def3f9c0a83657220c72223d8
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-09 06:23:30 +0100

    📦 Add IPFS storage provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,13 @@
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-ipfs-storage",
+      providerOptions: {
+        defaultStorage: "pinata",
+        pinata: { jwt: env("PINATA_JWT") },
+      },
+    },
+  },
   email: {
     config: {
       provider: "strapi-provider-email-mailjet",
