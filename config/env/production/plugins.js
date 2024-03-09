3522e0287466b372ab9a3d1483bf188c34422d5f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-09 07:05:49 +0100

    🔎 Add sentry

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -23,4 +23,11 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env("SENTRY_DSN"),
+      sendMetadata: true,
+    },
+  },
 });
