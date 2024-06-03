dfd3a8a6359e060bfecb05fbbe0ce63ef4da90bf
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-03 17:29:41 +0200

    :1234: article view table

---
+++ src/api/article-view/services/article-view.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-view service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::article-view.article-view');
