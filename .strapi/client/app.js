95164172be6c3d3b19f35f089e2aeef79504beb5
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-21 00:14:06 +0200

    :page_facing_up: cool diagram lol

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -4,7 +4,9 @@
  */
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
+import sentry from "@strapi/plugin-sentry/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import strapiContentTypeExplorer from "strapi-content-type-explorer/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 import customisations from "../../src/admin/app.js";
@@ -15,6 +17,8 @@ renderAdmin(document.getElementById("strapi"), {
   plugins: {
     graphql: graphql,
     i18n: i18N,
+    sentry: sentry,
     "users-permissions": usersPermissions,
+    "strapi-content-type-explorer": strapiContentTypeExplorer,
   },
 });
