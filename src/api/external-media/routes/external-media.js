84a1305611893719ae7f967951dbbe5ec0e57d99
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-10-06 21:45:40 +0200

    :sparkles: external media

---
+++ src/api/external-media/routes/external-media.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * external-media router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::external-media.external-media');
