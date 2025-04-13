0808b439e2a8a5c5aa7a15167588a6229752795d
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 11:02:46 +0200

    add last pull

---
+++ src/api/last-view-pull/controllers/last-view-pull.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * last-view-pull controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::last-view-pull.last-view-pull');
