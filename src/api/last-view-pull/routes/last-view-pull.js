5106cd7ea1e65fa8b74f8c32c128e21dfdcd058e
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 11:02:46 +0200

    add last pull

---
+++ src/api/last-view-pull/routes/last-view-pull.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * last-view-pull router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::last-view-pull.last-view-pull');
---
+++ src/api/last-view-pull/services/last-view-pull.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * last-view-pull service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::last-view-pull.last-view-pull');
