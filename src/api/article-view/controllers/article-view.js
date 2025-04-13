c1f2274ede643f0d9fc3374c8e7d833f100fc053
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 11:03:00 +0200

    removeu nused stuff

--- src/api/article-view/controllers/article-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-view controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::article-view.article-view');
