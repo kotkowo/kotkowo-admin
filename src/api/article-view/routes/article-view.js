9e5888dc31ce5de082fed3c149e51a41de28ac66
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 11:03:00 +0200

    removeu nused stuff

--- src/api/article-view/routes/article-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-view router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::article-view.article-view');
--- src/api/article-view/services/article-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-view service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::article-view.article-view');
